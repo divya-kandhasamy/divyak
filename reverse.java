ea1788cc013cc282376138424771c2ba61497850
divya-kandhasamy <divya1314123@gmail.com>
2016-08-19 13:57:08 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class reverse
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+StringBuffer str=new StringBuffer(s.nextLine());
+System.out.println(str.reverse());
+}
+}
